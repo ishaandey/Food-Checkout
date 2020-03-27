e5f534c4a2f66d47818edfb8cba9e01911e38246
ishaandey <ishaan.dey8@gmail.com>
2020-03-27 17:11:48 -0400

    idk

--- fridge_app/unsplash2.py
+++ fridge_app/unsplash2.py
@@ -1,6 +1,12 @@
-import json
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 11:49:44 2020
+
+@author: ishaandey
+"""
+
 import requests
-# import pandas as pd
 
 def get_food_img(thing='cheese'):
     try:
@@ -12,9 +18,10 @@ def get_food_img(thing='cheese'):
         }
         client_id = PARAMS['client_id']
         r = requests.get(url="https://api.unsplash.com/search/photos", params=PARAMS).json()
-        # print(r)
-        # df = pd.read_json(r['results'])
+        df = pd.read_json(r['results'])
         url = r['results'][0]['urls']['full']
     except:
         url = "https://images.unsplash.com/photo-1509440159596-0249088772ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80"
     return url
+
+get_food_img('blueberries')
